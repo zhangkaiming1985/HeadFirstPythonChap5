57a4679e792de31275dfcafde65c97eaf0d236c3
zhangkaiming1985 <zpoint@163.com>
2018-01-03 17:30:33 +0800

    macbook 2018-1-3 17:30

---
+++ athletemodel.py
@@ -0,0 +1,47 @@
+import pickle
+import func
+from cla_Athlete import AthleteList
+
+
+# 函数get_coach_data3,接受选手成绩文件名，去除前后空白，以，为分界符分割成绩，返回成绩列表。
+# chap6升级函数3，返回继承类
+
+def get_coach_data3(filename):
+	try:
+		with open(filename) as f:
+			data_list = f.readline().strip().split(',')
+			cla = AthleteList(data_list.pop(0), data_list.pop(0), data_list)
+			return cla
+	except IOError as io_err:
+		print("file error:" + str(io_err))
+		return None
+
+
+# 函数put_to_store(),按照名字为索引，将选手信息储存在字典中，之后腌制
+def put_to_store(files_list):
+	all_athletes = dict()
+	for each_file in files_list:
+		ath = get_coach_data3(each_file)
+		all_athletes[ath.name] = ath
+
+	try:
+		with open('athletes.pickle', 'wb') as athf:
+			pickle.dump(all_athletes, athf)
+	except IOError as ioerr:
+		print("file error:" + ioerr)
+	return all_athletes
+
+
+# 函数get_from_store(),将腌制的数据提取，放入字典并返回
+def push_from_store():
+	all_athletes = dict()
+	try:
+		with open('athletes,pickle', 'rb') as athf:
+			all_athletes = pickle.load(athf)
+	except IOError as ioerr:
+		print("file error:" + ioerr)
+	return all_athletes
+
+the_files = ["ch6_data/james2.txt", "ch6_data/julie2.txt", "ch6_data/mikey2.txt", "ch6_data/sarah2.txt"]
+data = put_to_store(the_files)
+print(data)
---
+++ cla_Athlete.py
@@ -0,0 +1,31 @@
+# 建立继承于list的类AthleteList
+import func
+
+
+class AthleteList(list):
+	def __init__(self, a_name, a_dob=None, a_times=[]):
+		list.__init__([])
+		self.name = a_name
+		self.dob = a_dob
+		self.extend(a_times)
+
+	def top3(self):
+		return sorted(set([func.sanitize(t) for t in self]))[0:3]
+
+
+# 函数get_coach_data3,接受选手成绩文件名，去除前后空白，以，为分界符分割成绩，返回成绩列表。
+# chap6升级函数3，返回继承类
+
+def get_coach_data3(filename):
+	try:
+		with open(filename) as f:
+			data_list = f.readline().strip().split(',')
+			cla = AthleteList(data_list.pop(0), data_list.pop(0), func.sanitize(data_list))
+			return cla
+	except IOError as io_err:
+		print("file error:" + str(io_err))
+		return None
+
+
+james = get_coach_data3("ch6_data/james2.txt")
+print(james.name + "'s fastest times are " + str(james.top3()))
